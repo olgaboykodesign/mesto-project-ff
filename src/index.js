631bbc19b0e5ee20a7a18cb80b3d2775fe64d375
olgaboykodesign <72735416+olgaboykodesign@users.noreply.github.com>
2024-11-28 16:03:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import { initialCards } from "./components/cards.js";
 import {
   openModal,
   closeModal,
-  closeModalOverlay,
-  closeModalButton,
+  setCloseModalByClickListeners,
 } from "./components/modal.js";
-import { createCard, deleteCard } from "./components/card.js";
+import { createCard, deleteCard, handleLikeClick } from "./components/card.js";
 import "./pages/index.css";
 
 const placesList = document.querySelector(".places__list");
@@ -13,31 +12,38 @@ const popup = document.querySelectorAll(".popup");
 const addButton = document.querySelector(".profile__add-button");
 const editButton = document.querySelector(".profile__edit-button");
 const popupNewCard = document.querySelector(".popup_type_new-card");
-const newPlaceButton = popupNewCard.querySelector(".popup__button");
 
-const newPlace = document.forms["new-place"];
+const newPlaceForm = document.forms["new-place"];
 const placeNameInput = document.querySelector(".popup__input_type_card-name");
 const linkInput = document.querySelector(".popup__input_type_url");
 
-const formElement = document.forms["edit-profile"];
+const editProfileForm = document.forms["edit-profile"];
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
 
 const popupEdit = document.querySelector(".popup_type_edit");
-const profileButton = popupEdit.querySelector(".popup__button");
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
+
+const popupImage = document.querySelector(".popup__image");
+const popupCaption = document.querySelector(".popup__caption");
+const imagePopupContainer = document.querySelector(".popup_type_image");
 
 //выводим карточки
 initialCards.forEach((el) =>
   placesList.append(createCard(el.name, el.link, deleteCard))
 );
 
-//Вешаем слушатели закрытия по Overlay и крестику на все popup
-popup.forEach((item) => {
-  item.addEventListener("click", closeModalOverlay);
-  item.addEventListener("click", closeModalButton);
-});
+// Функция для открытия попапа с изображением
+export function openImagePopup(name, link) {
+  popupImage.src = link;
+  popupImage.alt = name;
+  popupCaption.textContent = name;
+  openModal(imagePopupContainer);
+}
+
+// Закрытие попапов
+setCloseModalByClickListeners(popup);
 
 addButton.addEventListener("click", function () {
   openModal(popupNewCard); // открываем попап добавления
@@ -54,22 +60,20 @@ function formEditProfile(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   const name = nameInput.value;
   const about = jobInput.value;
-  profileButton.textContent = "Сохранение...";
   closeModal(popupEdit);
   profileTitle.textContent = name;
   profileDescription.textContent = about;
 }
 
-formElement.addEventListener("submit", formEditProfile);
-
 // Форма добавления нового места
-function addPlaceForm(evt) {
+function handlePlaceAddFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   const name = placeNameInput.value;
   const link = linkInput.value;
-  newPlaceButton.textContent = "Сохранение...";
   placesList.prepend(createCard(name, link, deleteCard));
+  evt.target.reset();
   closeModal(popupNewCard);
 }
 
-newPlace.addEventListener("submit", addPlaceForm);
+editProfileForm.addEventListener("submit", formEditProfile);
+newPlaceForm.addEventListener("submit", handlePlaceAddFormSubmit);
